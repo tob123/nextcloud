09d9145d9e483493c66eb4739f9b647f3bf86d8c
tob <tob@nice.eu>
2019-01-20 16:46:08 +0100

    update travis

---
+++ dockpush.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+set -ex
+docker push tob123/nextcloud-staging:${VERSION}
+if [ -n $LATEST_MINOR ]; then
+MAJOR_TAG=$(echo $VERSION | awk -F. {' print $1'})
+docker tag tob123/nextcloud-staging:${VERSION} tob123/nextcloud-staging:${MAJOR_TAG}
+docker push tob123/nextcloud-staging:${MAJOR_TAG}
+fi

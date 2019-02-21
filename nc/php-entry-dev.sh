b86e14aa1c28a7916e13158423a4281f3a856737
tob123 <tob@nice.eu>
2019-02-21 06:40:35 +0100

    dropped old redundant dockerfiles and fixed nextcloud upgrades

--- nc/php-entry-dev.sh
+++ nc/php-entry-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ex
+
 are_we_installed () {
 if [ -f /nextcloud/config/config.php ]; then
   echo "config file already exists. "
@@ -7,37 +8,30 @@ if [ -f /nextcloud/config/config.php ]; then
 fi
 }
 
-are_we_upgraded () {
-  db_status="notok"
-  counter=0
-  set +e
-  while [ $db_status = "notok" ] && [ $counter -le 30 ]; do
-    php /usr/local/dbcheck.php
-    if [ $? -ne 0 ]; then
-      sleep 1
-      counter=$(( $counter + 1 ))
-      else db_status="ok"
-    fi
-  done
-#run-parts -u 0007 /usr/local/ncup
-run-parts /usr/local/ncup
-set -e
-#fi
-}
-
-install_nc () {
-#umask 0007
+check_db () {
 db_status="notok"
 counter=0
 set +e
 while [ $db_status = "notok" ] && [ $counter -le 30 ]; do
-  php /usr/local/dbcheckenv.php
+  php /usr/local/${PHP_CHK_FILE}
   if [ $? -ne 0 ]; then
     sleep 1
     counter=$(( $counter + 1 ))
-  else db_status="ok"
+    else db_status="ok"
   fi
 done
+}
+
+are_we_upgraded () {
+PHP_CHK_FILE=dbcheck.php
+check_db
+occ upgrade
+set -e
+}
+
+install_nc () {
+PHP_CHK_FILE=dbcheckenv.php
+check_db
 cp /usr/local/config.php /nextcloud/config/config.php
 /usr/local/bin/ncinstall.sh
 set -e
@@ -53,26 +47,18 @@ if [ $AUTO_CONV_FC = "YES" ]; then
 fi
 }
 
-tunables () {
-#echo "MEMORY_LIMIT=${MEMORY_LIMIT}" > /home/webadm/.profile
-echo ""
-}
-
 am_i_webadm () {
 if [ $(whoami) = apache ]; then
   nc_installed="no"
   are_we_installed
   # Put the configuration and apps into volumes
   if [ $nc_installed = "no" ]; then
-#    umask 0007
     install_nc
-#    chmod 660 /nextcloud/config/config.php
-    tunables
   fi
   if [ $nc_installed = "yes" ]; then
+    if [ $NC_UP_AUTO = "yes" ]; then
     are_we_upgraded
-    tunables
-#    exit 0
+    fi
   fi
 fi
 }
@@ -80,7 +66,4 @@ fi
 # define some conditions on whether we are installed or not
 am_i_webadm
 #startup
-#umask 0007
-#hacks to reset permissions to apache user for apps that have been installed. otherwise the nextcloud status board complains
-#find /nextcloud/apps2 -user webadm -maxdepth 1 -mindepth 1 -exec cp -dR {} {}.org \; -exec rm -rf {} \; -exec mv {}.org {} \;
 exec "$@"

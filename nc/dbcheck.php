1ede0e8d6343e036a575a6afa32d944802fc6fc6
tob <tob@nice.eu>
2020-06-06 10:31:32 +0200

    fix dbcheck for postgres

--- nc/dbcheck.php
+++ nc/dbcheck.php
@@ -8,10 +8,11 @@ $password = $CONFIG['dbpassword'];
 $username = $CONFIG['dbuser'];
 $dbtype = $CONFIG['dbtype'];
 $servername = $CONFIG['dbhost'];
+$db_name = $CONFIG['dbname'];
  //
 // Create connection
 try {
-    $conn = new PDO("$dbtype:host=$servername", $username, $password);
+    $conn = new PDO("$dbtype:host=$servername;dbname=$db_name", $username, $password);
     $conn = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";

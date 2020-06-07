14c120d52e92ec9de32ef2fdb54da1957634d708
tob <tob@nice.eu>
2020-06-07 22:20:44 +0200

    initiate push to repo only for fastest build_test type (sqlite)

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 set -ex
 STG_REPO="docker.io/tob123/nextcloud-staging"
-if [[ -n $LATEST ]]; then
+if [[ -n $LATEST && ${DB_TYPE_CI} = "sqlite" ]]; then
 		docker buildx build --platform linux/amd64,linux/arm/v7 \
 		--build-arg NC_VER=${VERSION} \
 		--tag ${STG_REPO}:${VERSION} \
@@ -10,9 +10,25 @@ if [[ -n $LATEST ]]; then
 		--push --progress plain nc
 	exit 0
 fi
-docker buildx build \
---platform linux/amd64,linux/arm/v7 \
+if [[ ${DB_TYPE_CI} = "sqlite" ]]; then
+  docker buildx build \
+  --platform linux/amd64,linux/arm/v7 \
+  --build-arg NC_VER=${VERSION} \
+  --tag ${STG_REPO}:${VERSION} \
+  --tag ${STG_REPO}:${VERSION_MAJOR} \
+  --push --progress plain nc
+  exit 0
+fi
+if [[ -n $LATEST ]]; then
+  docker build \
+  --build-arg NC_VER=${VERSION} \
+  --tag ${STG_REPO}:${VERSION} \
+  --tag ${STG_REPO}:${VERSION_MAJOR} \
+  --tag ${STG_REPO}:latest nc
+  exit 0
+fi
+docker build \
 --build-arg NC_VER=${VERSION} \
 --tag ${STG_REPO}:${VERSION} \
---tag ${STG_REPO}:${VERSION_MAJOR} \
---push --progress plain nc
+--tag ${STG_REPO}:${VERSION_MAJOR} nc
+exit 0
